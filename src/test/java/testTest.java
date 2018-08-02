1c970e9ecb3284e780afbcbda5f8de8aaee29b12
pol17 <pol17@ukr.net>
2018-08-02 21:00:16 +0200

    [QA] Log4j Works

---
+++ src/test/java/testTest.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class testTest {
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+
+    public void test () {
+        logger.info("test");
+    }
+}
